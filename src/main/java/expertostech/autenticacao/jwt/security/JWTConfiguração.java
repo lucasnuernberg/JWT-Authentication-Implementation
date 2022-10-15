700cdee6d62658de4684d76610d364ae04b2217d
lucasnuernberg <lucasnogoncalves@hotmail.com>
2022-10-15 14:33:59 -0300

    Configuração do CORS

---
+++ src/main/java/expertostech/autenticacao/jwt/security/JWTConfiguração.java
@@ -0,0 +1,56 @@
+package expertostech.autenticacao.jwt.security;
+
+import expertostech.autenticacao.jwt.service.DetalheUsuarioServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@EnableWebSecurity
+public class JWTConfiguração extends WebSecurityConfigurerAdapter {
+
+    private final DetalheUsuarioServiceImpl usuarioService;
+    private final PasswordEncoder passwordEncoder;
+
+    public JWTConfiguração(DetalheUsuarioServiceImpl usuarioService, PasswordEncoder passwordEncoder) {
+        this.usuarioService = usuarioService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService).passwordEncoder(passwordEncoder);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST, "/login").permitAll() // Permitindo qualquer acesso a essa rota
+                .anyRequest().authenticated() // Para as demais precisa de AUTH
+                .and()
+                .addFilter(new JWTAutenticarFilter(authenticationManager()))
+                .addFilter(new JWTValidarFilter(authenticationManager()))
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Desebilitando por se travar de um ambiente de desenvolvimento | proteção de ataques | Ativar na produção
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        // Configuração para todos os dominios possam fazer requisições
+
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+
+        return source;
+    }
+
+}
